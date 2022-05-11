ea7d8271342db6a5d354b8a0c2cfcdb93c3ef068
StefanBucei <gb222kh@student.lnu.se>
2022-05-11 16:27:52 +0200

    Testning - första utkast

--- src/main/java/com/example/libraryproject/TestPackage/TestGubbe.java
+++ src/main/java/com/example/libraryproject/TestPackage/TestGubbe.java
@@ -74,7 +74,7 @@ public class TestGubbe extends Application {
         sun.setStrokeWidth(5);
         sun.setFill(Color.YELLOW);
 
-        //Hoppas det fungerar rr
+        //Hoppas det fungerar rrdsfdsfs
 
         root.getChildren().addAll(back,bottom,middle,top,button1,button2,button3,eye1,eye2,smile,sun);
 
