f87a185af24d2b89c24ad689314fb24e3f10a74e
StefanBucei <gb222kh@student.lnu.se>
2022-05-11 15:29:35 +0200

    Testning - första utkast

---
+++ src/main/java/Test/Average.java
@@ -0,0 +1,24 @@
+package Test;
+
+public class Average implements Testning{
+
+    public int[] listArray;
+
+    @Override
+    public double getAverage() {
+        int length = listArray.length;
+        int sum = 0;
+
+
+        for (int i = 0; i< listArray.length; i++){
+            sum += listArray[i];
+        }
+        if (listArray.length == 0) {
+            throw new ArithmeticException("Listan är tom");
+        }
+
+        double average = sum/length;
+
+        return average;
+    }
+}
---
+++ src/main/java/Test/Testning.java
@@ -0,0 +1,5 @@
+package Test;
+
+public interface Testning {
+    double getAverage();
+}
