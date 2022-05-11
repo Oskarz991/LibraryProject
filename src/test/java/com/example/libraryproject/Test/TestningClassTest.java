de8e27909344891c1a1833487eacaede2f9e8647
Oskarz991 <oskarz991@gmail.com>
2022-05-11 15:52:08 +0200

    Uppdatering 2

--- src/test/java/com/example/libraryproject/Test/TestningClassTest.java
+++ src/test/java/com/example/libraryproject/Test/TestningClassTest.java
@@ -13,4 +13,13 @@ class TestningClassTest {
 
             assertEquals( 25,average.getAverage());
         }
+
+    @Test
+    void getAverage2() {
+        TestningClass average = new TestningClass();
+        average.listArray = new int[] {10,20,30,40};
+
+        assertEquals( 25,average.getAverage());
+    }
+
     }
