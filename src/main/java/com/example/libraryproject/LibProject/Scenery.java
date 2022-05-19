d23fd339e852efa153965c4a6946cb2cdd0fb620
StefanBucei <gb222kh@student.lnu.se>
2022-05-19 12:00:38 +0200

    Fixade layout - fjärde utkast

--- src/main/java/com/example/libraryproject/LibProject/Scenery.java
+++ src/main/java/com/example/libraryproject/LibProject/Scenery.java
@@ -8,13 +8,10 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
@@ -30,70 +27,96 @@ public class Scenery extends Application {
             VBox root = new VBox();
             root.setSpacing(5);
             root.setPadding(new Insets(5, 5, 10, 5));
-            Label title = new Label("Library system");
-            title.setFont(new Font("Fira Sans", 32));
+            Label titleLibrarian = new Label("Login as a libriarian");
+            Label titleMember = new Label("Login as a member");
+            Label titleNewAccount = new Label("Register new account");
+            titleLibrarian.setFont(new Font("Fira Sans", 32));
+            titleMember.setFont(new Font("Fira Sans", 32));
+            titleNewAccount.setFont(new Font("Fira Sans", 32));
 
-            GridPane pane = new GridPane();
-            pane.setAlignment(Pos.BASELINE_LEFT);
-            pane.setHgap(5);
-            pane.setVgap(5);
+            SplitPane splitPane = new SplitPane();
 
-            Label lblBookTitle = new Label("Book title:");
-            TextField name = new TextField();
-            Label lblISPM = new Label("ISPM:");
-            TextField utgvAr = new TextField();
+            VBox leftControl  = new VBox(new Label("Left Control"));
+            VBox rightControl = new VBox(new Label("Right Control"));
+
+            GridPane paneLeft = new GridPane();
+            paneLeft.setAlignment(Pos.BASELINE_LEFT);
+            paneLeft.setHgap(5);
+            paneLeft.setVgap(5);
+
+            GridPane paneRight = new GridPane();
+            paneRight.setAlignment(Pos.BASELINE_LEFT);
+            paneRight.setHgap(5);
+            paneRight.setVgap(5);
+
+            //Logga in som medlem
+            Label nameMemberLbl = new Label("Name:");
+            TextField nameMemberTxt = new TextField();
+            Label idMemberLbl = new Label("ID-number:");
+            TextField idMemberTxt = new TextField();
+
+            //Skapa nytt konto
+            Label surnameMemberLbl = new Label("Surname:");
+            TextField surnameMemberTxt = new TextField();
+            surnameMemberLbl.setVisible(false);
+            Label personalNrMemberLbl = new Label("Personal Number:");
+            TextField personalNrMemberTxt = new TextField();
+            personalNrMemberLbl.setVisible(false);
+            personalNrMemberTxt.setVisible(false);
 
             //Lägg till en ny title
-            Label lblBildades = new Label("Name:");
-            TextField bildades = new TextField();
-            Label lblPersonalNr = new Label("Personal number:");
-            TextField genre = new TextField();
 
-            lblBildades.setVisible(false);
-            bildades.setVisible(false);
-            lblPersonalNr.setVisible(false);
-            genre.setVisible(false);
+            Button registerNewAccount = new Button("Register new account ");
+            Button loginAsAMember = new Button("Login as a member");
+            loginAsAMember.setVisible(false);
 
-            Button searchForBook = new Button("Search for book by title ");
-            Button skapaArtist2 = new Button("Register new account");
-            skapaArtist2.setVisible(false);
-
-            Button insertAlbum = new Button("Search book ");
-            Label albumTillad = new Label();
-            Button insertArtist = new Button("Register account");
-            insertArtist.setVisible(false);
+            Button loginBtn = new Button("Login ");
+            Button registerBtn = new Button("Register account");
+            registerBtn.setVisible(false);
 
             ComboBox<String> artister = new ComboBox<>();
 
-            searchForBook.setOnAction(e-> {
-                name.clear(); utgvAr.clear();
-                searchForBook.setVisible(false); skapaArtist2.setVisible(true);
-                insertAlbum.setVisible(false); insertArtist.setVisible(true);
-                lblBookTitle.setVisible(true); name.setVisible(true);
-                lblISPM.setVisible(false); utgvAr.setVisible(false);
-                lblBildades.setVisible(true);bildades.setVisible(true);
-                lblPersonalNr.setVisible(true);genre.setVisible(true);
+            registerNewAccount.setOnAction(e-> {
+                nameMemberTxt.clear(); idMemberTxt.clear();
+                registerNewAccount.setVisible(false); loginAsAMember.setVisible(true);
+                loginBtn.setVisible(false); registerBtn.setVisible(true);
+                nameMemberTxt.setVisible(true); nameMemberTxt.setVisible(true);
+                idMemberLbl.setVisible(false); idMemberTxt.setVisible(false);
+                surnameMemberLbl.setVisible(true); surnameMemberTxt.setVisible(true);
+                personalNrMemberLbl.setVisible(true); personalNrMemberTxt.setVisible(true);
             });
 
-            skapaArtist2.setOnAction(e-> {
-                name.clear(); bildades.clear(); genre.clear();
-                searchForBook.setVisible(true); skapaArtist2.setVisible(false);
-                insertAlbum.setVisible(true); insertArtist.setVisible(false);
-                lblBookTitle.setVisible(true); name.setVisible(true);
-                lblISPM.setVisible(true); utgvAr.setVisible(true);
-                lblBildades.setVisible(false);bildades.setVisible(false);
-                lblPersonalNr.setVisible(false);genre.setVisible(false);
+            loginAsAMember.setOnAction(e-> {
+                nameMemberTxt.clear();
+                registerNewAccount.setVisible(true); loginAsAMember.setVisible(false);
+                loginBtn.setVisible(true); registerBtn.setVisible(false);
+                nameMemberTxt.setVisible(true); nameMemberTxt.setVisible(true);
+                idMemberLbl.setVisible(true); idMemberTxt.setVisible(true);
+                surnameMemberLbl.setVisible(false); surnameMemberTxt.setVisible(false);
+                personalNrMemberLbl.setVisible(false); personalNrMemberTxt.setVisible(false);
+
             });
+            //Lägg till på vänster sida
+            paneLeft.add(titleLibrarian,0,0);
+            paneLeft.add(artister,0,1);
 
-            pane.add(artister,0,0); pane.add(searchForBook,1,0); pane.add(skapaArtist2,1,0);
-            pane.add(lblBookTitle,0,1); pane.add(name,1,1);
-            pane.add(lblISPM,0,2); pane.add(utgvAr,1,2);
-            pane.add(lblBildades,0,2); pane.add(bildades,1,2);
-            pane.add(lblPersonalNr,0,3); pane.add(genre,1,3);
-            pane.add(insertAlbum,0,4); pane.add(insertArtist,0,4);
 
-            root.getChildren().addAll(title,albumTillad,pane);
-            Scene scene = new Scene(root ,500,400);
+            //Lägg till på höger sida
+            paneRight.add(titleMember,0,0);
+            paneRight.add(loginAsAMember,0,1); paneRight.add(registerNewAccount,0,1);
+            paneRight.add(nameMemberLbl,0,2); paneRight.add(nameMemberTxt,1,2);
+            paneRight.add(idMemberLbl,0,3); paneRight.add(idMemberTxt,1,3);
+            paneRight.add(personalNrMemberLbl,0,4); paneRight.add(personalNrMemberTxt,1,4);
+            paneRight.add(surnameMemberLbl,0,3); paneRight.add(surnameMemberTxt,1,3);
+            paneRight.add(loginBtn,1,6); paneRight.add(registerBtn,1,6);
+
+
+            leftControl.getChildren().addAll(paneLeft);
+            rightControl.getChildren().addAll(paneRight);
+
+            splitPane.getItems().addAll(leftControl,rightControl);
+            root.getChildren().addAll(splitPane);
+            Scene scene = new Scene(root ,900,500);
             primaryStage.setTitle("Library");
             primaryStage.setScene(scene);
             primaryStage.show();
