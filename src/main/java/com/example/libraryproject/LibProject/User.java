d43401ab7cbd185ebeb459f64c889ebecf0d8659
Oskarz991 <oskarz991@gmail.com>
2022-05-23 17:36:29 +0200

    SearchBook i User finished

--- src/main/java/com/example/libraryproject/LibProject/User.java
+++ src/main/java/com/example/libraryproject/LibProject/User.java
@@ -1,8 +1,12 @@
 package com.example.libraryproject.LibProject;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
 
 public class User {
 
@@ -14,6 +18,11 @@ public class User {
     public int LoanCounter;
     public int ViolationCounter;
     public LocalDate Timer;
+    public File AllBooksFile = new File("src/main/java/com/example/libraryproject/LibProject/AllBooks.txt");
+    public File UserLoanFile = new File("src/main/java/com/example/libraryproject/LibProject/LoanedBooks.txt");
+    public File UserFile = new File("src/main/java/com/example/libraryproject/LibProject/WhiteList.txt");
+    public File blackFile = new File("src/main/java/com/example/libraryproject/LibProject/BlackList.txt");
+    public File timeoutFile = new File("src/main/java/com/example/libraryproject/LibProject/TimeoutList.txt");
 
    public User(String name,String surname,int pnumber,int id,int loancounter,int violationcounter,String role){
         this.Name = name;
@@ -103,11 +112,31 @@ public class User {
         ViolationCounter = violationCounter;
     }
 
-    public Book searchTitle(String Title, int id){
+    public Book searchTitle(String title)throws IOException {
 
-        Book temp = new Book();
+        Scanner bookScan = new Scanner(AllBooksFile).useDelimiter(",");
 
-        return temp;
+        ArrayList<Book> bookList = new ArrayList<Book>();
+
+        while (bookScan.hasNext()) {
+            int Id = Integer.parseInt(bookScan.next());
+            String Name = bookScan.next();
+            int ISBN = Integer.parseInt(bookScan.next());
+            int Quantity = Integer.parseInt(bookScan.next());
+            String Author = bookScan.nextLine();
+            Author = Author.replace(",", "");
+
+            bookList.add(new Book(Id, Name, ISBN, Quantity, Author));
+        }
+
+        Book theBook = new Book();
+
+        for (Book book : bookList) {
+            if (title.equals(book.Title)) {
+                theBook = book;
+            }
+        }
+        return theBook;
     }
 
     public void requestDelete(int id){
@@ -139,4 +168,14 @@ public class User {
 
         return  user.Name + "," + user.Surname + "," + user.PNumber + "," + user.Id + "," + user.LoanCounter + "," + user.ViolationCounter + "," + user.Timer + "," + user.Role;
    }
+
+
+    public static void main(String[] args)throws IOException {
+       User testUser = new User();
+       Book testBook = new Book();
+
+       testBook = testUser.searchTitle("Oskars resor");
+       System.out.println(testBook.Title);
+
+    }
 }
